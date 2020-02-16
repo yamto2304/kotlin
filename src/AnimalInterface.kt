114572029ec6af3f06fd7460da883c11a9fc58ee
Ymt <tuanit2304@gmail.com>
2020-02-16 16:13:00 +0700

    10 - learn about interface

---
+++ src/AnimalInterface.kt
@@ -0,0 +1,10 @@
+package hello
+
+interface AnimalInterface{
+    fun walk(){
+        println("I can walk")
+    }
+    fun eat(){
+        println("I'm animal, i can eat")
+    }
+}
---
+++ src/BirdInterface.kt
@@ -0,0 +1,10 @@
+package hello
+
+interface BirdInterface {
+    fun fly(){
+        println("I can fly")
+    }
+    fun eat(){
+        println("I'm a bird. I can eat")
+    }
+}
